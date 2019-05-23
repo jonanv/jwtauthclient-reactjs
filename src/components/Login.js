c98cc43c9414a17f89419f8290c82f5d0717bb52
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-05-22 21:52:15 -0500

    Se hizo login con material-ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,39 +1,95 @@
-import React, { Component } from 'react';
-import './Login.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 import logo from '../logo.svg';
 
-class Login extends Component {
-    render() {
-        return (
-            <div className="center">
-                <img src={logo} className="App-logo" alt="logo" />
-                <div className="card">
-                    <h1>Login</h1>
-                    <form onSubmit={this.handleFormSubmit}>
-                        <input
-                            className="form-item"
-                            placeholder="Username goes here..."
-                            name="username"
-                            type="text"
-                            onChange={this.handleChange}
-                        />
-                        <input
-                            className="form-item"
-                            placeholder="Password goes here..."
-                            name="password"
-                            type="password"
-                            onChange={this.handleChange}
-                        />
-                        <input
-                            className="form-submit"
-                            value="SUBMIT"
-                            type="submit"
-                        />
-                    </form>
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    img: {
+        animation: 'App-logo-spin infinite 20s linear',
+        height: '15vmin',
+        pointerEvents: 'none',
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+        backgroundColor: '#282c34',
+    },
+});
+
+function Login(props) {
+    const { classes } = props;
+
+    return (
+        <main className={classes.main}>
+            <CssBaseline />
+            <Paper className={classes.paper}>
+                <div>
+                    <img src={logo} alt="logo" className={classes.img} />
                 </div>
-            </div>
-        );
-    }
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <form className={classes.form}>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="username">Username</InputLabel>
+                        <Input id="username" name="username" autoComplete="username" autoFocus />
+                    </FormControl>
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <Input name="password" type="password" id="password" autoComplete="current-password" />
+                    </FormControl>
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="default" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign in
+                    </Button>
+                </form>
+            </Paper>
+        </main>
+    );
 }
 
-export default Login;
+Login.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
