ee15a8a1b56f5dd8f64633306ff4a402804f5171
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-05-22 21:03:51 -0500

    Se hizo login para a autentificación

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './Login.css';
+import logo from '../logo.svg';
 
 class Login extends Component {
-    render(){
-        return(
-            <div>
-                Login
+    render() {
+        return (
+            <div className="center">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="card">
+                    <h1>Login</h1>
+                    <form onSubmit={this.handleFormSubmit}>
+                        <input
+                            className="form-item"
+                            placeholder="Username goes here..."
+                            name="username"
+                            type="text"
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            className="form-item"
+                            placeholder="Password goes here..."
+                            name="password"
+                            type="password"
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            className="form-submit"
+                            value="SUBMIT"
+                            type="submit"
+                        />
+                    </form>
+                </div>
             </div>
         );
     }
