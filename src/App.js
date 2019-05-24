a49a08651397db70c8f4535f82945e16b088e370
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-05-23 23:35:57 -0500

    Se hicieron modificaciones en el panel, se modifico el boton con material-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 import AuthService from './utils/AuthService';
 import withAuth from './utils/withAuth';
@@ -15,20 +16,24 @@ class App extends Component {
     }
 
     render(){
+        const { classes } = this.props;
+
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <p>
-                        Welcome {this.props.user.username}
+                        Welcome <code><h3>{this.props.user.username}</h3></code>
                     </p>
-                    <button
-                        type="button"
-                        className="form-submit"
+
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
                         onClick={this.handleLogout.bind(this)}
                     >
                         Logout
-                    </button>
+                    </Button>
                 </header>
             </div>
         );
